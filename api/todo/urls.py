d4a410b4bf0c7195039da2744f47cd2dbcd10591
kbanchi1111 <kbanchi.1111@gmail.com>
2024-01-24 00:56:51 +0900

    add method base views and urls

--- api/todo/urls.py
+++ api/todo/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.urls import include
 from rest_framework.routers import DefaultRouter
 from todo.views import TodoViewSet
+from todo.views import todo_list
 
 router = DefaultRouter()
 router.register("todos", TodoViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("list/", todo_list)
 ]
--- api/todo/views.py
+++ api/todo/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from todo.models import Todo
 from todo.serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+
+@api_view(["GET"])
+def todo_list(request):
+    if request.method == "GET":
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
